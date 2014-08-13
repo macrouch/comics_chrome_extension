2b71e184674644f2daaea7807449f84e3496f3da
Matthew Crouch <matthew.a.crouch@gmail.com>
2014-08-13 17:36:17 -0400

    missed a file in the last commit

---
+++ js/popup.js
@@ -0,0 +1,34 @@
+show_username();
+
+function clickHandler(element) {
+  var issue_id = '';
+  var volume_id = '';
+  chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
+    var url = tab[0].url;
+
+    if (element.toElement.id == "add-volume") {
+      volume_id = url.replace('http://www.comicvine.com/', '').split('/')[1];
+
+      addVolume(volume_id)
+    } else if (element.toElement.id == 'remove-volume') {
+      volume_id = url.replace('http://www.comicvine.com/', '').split('/')[1];
+
+      removeVolume(volume_id);
+    } else if (element.toElement.id == 'add-issue') {
+      issue_id = url.replace('http://www.comicvine.com/', '').split('/')[1];
+
+      addIssue(issue_id);
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#add-issue').addEventListener('click', clickHandler);
+  document.querySelector('#add-volume').addEventListener('click', clickHandler);
+  document.querySelector('#remove-volume').addEventListener('click', clickHandler);
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
+    setPopupButtons(tab);
+  });
+
+})
